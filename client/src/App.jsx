6aea0ed3f6ac6d474c29ed3adb80f2a3ab74deef
malitha <it22324238@my.sliit.lk>
2024-11-18 17:11:21 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,14 @@ import Signin from "./pages/signin"
 import Signout from "./pages/signout"
 import About from "./pages/about"
 import Profile from "./pages/Profile"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes> 
         <Route path="/" element={<Home/>}></Route>
         <Route path="/sign-in" element={<Signin/>}></Route>
@@ -16,6 +19,7 @@ export default function App() {
         <Route path="/about-us" element={<About/>}></Route>
         <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
